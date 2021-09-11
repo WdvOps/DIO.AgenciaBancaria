22c44e579937f20ce4ac30e1ac97d401e5aabe63
WdvOps <dvops@outlook.com>
2021-09-10 23:58:40 -0300

    BUG]

---
+++ AgenciaBancaria.App/Program.cs
@@ -0,0 +1,27 @@
+﻿using AgenciaBancaria.Dominio;
+using System;
+
+
+namespace AgenciaBancaria.App
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Endereco endereco = new Endereco("Rua teste", "3333330", "Belo Horizonte", "MG");
+                Cliente cliente = new Cliente("Bruna", "123456", "780910", endereco);
+
+                ContaBancaria conta = new ContaBancaria(cliente);
+
+                Console.WriteLine("Conta criada: " + conta.NumeroConta + "-" + conta.DigitoVerificador);
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+           
+        }
+    }
+}
